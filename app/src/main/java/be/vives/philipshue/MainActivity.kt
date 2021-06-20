b1ce6543b703a8f01dff4159a7ad0abe956510d7
Hans Naert <hans.naert@vives.be>
2021-06-20 11:44:41 +0200

    JsonObjectRequest and readout lamp1 state

--- app/src/main/java/be/vives/philipshue/MainActivity.kt
+++ app/src/main/java/be/vives/philipshue/MainActivity.kt
@@ -7,8 +7,10 @@ import be.vives.philipshue.databinding.ActivityMainBinding
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     // Instantiate the RequestQueue.
     lateinit var queue : RequestQueue;
-    val get_url = "http://httpbin.org/get?param_a=2"
+    val get_url = "http://10.198.120.60/api/QScScRGIUH581BZOxzAoTrW76rN38GfgXd9QIFyz/lights"
     val put_url = "http://httpbin.org/put?param_ap=2"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,15 +32,17 @@ class MainActivity : AppCompatActivity() {
             Log.i("TEST", "Get button pressed")
             Log.i("TEST", "Get URL: "+get_url)
             // Request a string response from the provided URL.
-            val stringRequest = StringRequest(
-                Request.Method.GET, get_url,
-                Response.Listener<String> { response ->
-                    binding.textView.setText("Response is: ${response}")
+            val jsonObjectRequest = JsonObjectRequest(
+                Request.Method.GET, get_url,null,
+                Response.Listener<JSONObject> { response ->
+                    val lamp1=response.getJSONObject("1")
+                    val lamp1State=lamp1.getJSONObject("state")
+                    binding.textView.setText("lamp 1 is on: ${lamp1State.getBoolean("on")}")
                 },
                 Response.ErrorListener { error-> binding.textView.setText("That didn't work! Error is: ${error.toString()}") })
 
             // Add the request to the RequestQueue.
-            queue.add(stringRequest)
+            queue.add(jsonObjectRequest)
         }
 
 
